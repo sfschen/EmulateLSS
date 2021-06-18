24aede2c14e5d37e7caaeb0788c23d9cb700e296
Martin J White <mwhite@berkeley.edu>
2021-06-18 15:01:33 -0700

    More Ps, new file names.

--- build_training_set.py
+++ build_training_set.py
@@ -24,6 +24,7 @@ class AnzuParameters():
     """Returns a vector of parameters to sample at.
        This should be coordinated with the theory module being used,
        in this case for Anzu the parameters we can vary are:
+           zfid:  zfid
            cosmo: wb,wc,ns,sig8,hub
            bias:  b1,b2,bs,bn
            stoch: sn
@@ -36,7 +37,6 @@ class AnzuParameters():
         self.pmin = pmin
         self.pmax = pmax
         ndim      = np.sum(np.abs(pmax-pmin)>0)
-        if rank==0: print("ndim=",ndim)
         self.qrs  = QuasiRandomSequence(ndim)
     def sample(self,n):
         """Gets the n'th vector of parameter values."""
@@ -55,30 +55,39 @@ def generate_models(params,nstart=0,nend=10):
     """Does the work of generating the model predictions.  This also
        needs to know something about what is passed upon model class
        creation and what is a parameter to the model's __call__ method."""
-    par = None
-    mod = None
+    par,kvv,pgg,pgm,pmm = None,None,None,None,None
     for n in range(nstart,nend):
         if n%nproc==rank:
             pvec = params.sample(n)
             if par is None:
                 par = np.zeros( (nend-nstart,pvec.size) )
             model= AnzuPowerSpectra(pvec[0],pvec[1:6])
-            thy  = model(pvec[6:])[1] # Just select Pgg as an example.
-            if mod is None:
-                mod = np.zeros( (nend-nstart,thy.size) )
+            thy  = model(pvec[6:])
+            if kvv is None:
+                kvv = thy[0].copy() # Only need one copy.
+                pgg = np.zeros( (nend-nstart,thy[1].size) )
+                pgm = np.zeros( (nend-nstart,thy[2].size) )
+                pmm = np.zeros( (nend-nstart,thy[3].size) )
             par[n,:] = pvec.copy()
-            mod[n,:] = thy.copy()
-    # Just reduce everything to process 0 for convenience.
-    ptot,mtot = np.zeros_like(par),np.zeros_like(mod)
-    comm.Reduce(par,ptot,op=MPI.SUM,root=0)
-    comm.Reduce(mod,mtot,op=MPI.SUM,root=0)
+            pgg[n,:] = thy[1].copy()
+            pgm[n,:] = thy[2].copy()
+            pmm[n,:] = thy[3].copy()
+    # Just reduce everything to process 0 for convenience,
+    # memory shouldn't be an issue.
+    res = [kvv]
+    for x in [par,pgg,pgm,pmm]:
+        tot = np.zeros_like(x)
+        comm.Reduce(x,tot,op=MPI.SUM,root=0)
+        if rank==0: res.append(tot.copy())
     # Now write the answers to disk.  We have the choice here to
     # include constant parameters or exclude them.  I am including
     # them, as they can be easily removed later by selecting on
     # parameters with np.std==0.
     if rank==0:
-        np.savetxt("par.txt.gz",ptot)
-        np.savetxt("mod.txt.gz",mtot)
+        fb = "anzu_{:07d}_{:07d}".format(nstart,nend)
+        # For now just use gzipped ascii, this could change.
+        for x,n in zip(res,["kvv","par","pgg","pgm","pmm"]):
+            np.savetxt(fb+"_"+n+".txt.gz",x)
     #
 
 
