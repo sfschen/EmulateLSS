0c1c0b5a7ce5c0a0802ac46c6ad52f53e5565f29
Joe DeRose <joe.derose13@berkeley.edu>
2021-06-30 15:54:03 -0700

    fast/slow training data generation, parallelized file writing

--- generate_training_data.py
+++ generate_training_data.py
@@ -34,58 +34,95 @@ class AnzuParameters():
         return pars
 
 
-def generate_models(params, param_names, model, emu_info, nstart=0, nend=10):
+def generate_models(params, param_names, model, emu_info, nstart=0, nend=10,
+                    params_fast=None, param_names_fast=None, nfast_per_slow=1):
 
+    if params_fast is None:
+        allslow = True
+        if nfast_per_slow != 1:
+            print('Setting nfast_per_slow=1')
+            nfast_per_slow = 1
+    else:
+        allslow = False
+        assert(nfast_per_slow > 1) #otherwise no point in fast
+        
     npars = nend - nstart
-    out = {'params': np.zeros((npars, params.ndim))}
-
-    for n in range(nstart, nend):
+    
+    if param_names_fast is not None:
+        param_names_all = param_names + param_names_fast
+    else:
+        param_names_all = params_names
+        
+    npars_slow = (npars + nfast_per_slow - 1) // nfast_per_slow
+    npars_this = ((npars_slow + nproc - 1) // nproc) * nfast_per_slow
+    
+    out = {'params': np.zeros((npars_this, len(param_names_all)))}    
+    count = 0
+    for n in range(npars_slow):
         if n % nproc == rank:
-            pvec = params.sample(n)
-            pars = dict(zip(param_names, pvec))
-            out['params'][n] = pvec
-            model.logposterior(pars)
             if rank==0:
                 print(n)
                 sys.stdout.flush()
-                
-            if 'provider' in emu_info:
-                for thy in emu_info['provider']:
-                    pred = model.provider.get_result(thy)
-                    
-                    if thy not in out:
-                        tsize = [npars]
-                        [tsize.append(d) for d in pred.shape]
-#                        tsize = [d for d in pred.shape]
-#                        tsize.prepend(npars)
-                        out[thy] = np.zeros(tsize)
-                    out[thy][n] = pred
 
-            if 'likelihood' in emu_info:
-                for like in emu_info['likelihood']:
-                    attr = emu_info['likelihood'][like]
-                    pred = getattr(model.likelihood[like], attr)
-                    name = '{}.{}'.format(like, attr)
-                    if name not in out:
-                        tsize = [npars]
-                        [tsize.append(d) for d in pred.shape]
-                        out[name] = np.zeros(tsize)
-                    out[name][n] = pred
+            pslow = params.sample(n)
+            for m in range(nfast_per_slow):
+                if not allslow:
+                    fvec = params_fast.sample(n * nfast_per_slow + m)
+                    pvec = np.concatenate([pslow, fvec])
+                else:
+                    pvec = pslow
 
-    for k in out:
-        tot = np.zeros_like(out[k])
-        comm.Reduce(out[k], tot, op=MPI.SUM, root=0)
-        out[k] = tot
+                pars = dict(zip(param_names_all, pvec))
+                out['params'][count] = pvec
+                model.logposterior(pars)
 
-    if rank == 0:
-        with h5py.File(emu_info['output_filename'], 'w') as fp:
+                if 'provider' in emu_info:
+                    for thy in emu_info['provider']:
+                        pred = model.provider.get_result(thy)
+                        
+                        if thy not in out:
+                            tsize = [npars_this]
+                            [tsize.append(d) for d in pred.shape]
+                            out[thy] = np.zeros(tsize)
+                        out[thy][count] = pred
 
+                if 'likelihood' in emu_info:
+                    for like in emu_info['likelihood']:
+                        if hasattr(emu_info['likelihood'][like], '__iter__'):
+                            for l in emu_info['likelihood'][like]:
+                                pred = getattr(model.likelihood[like], l)
+                                name = '{}.{}'.format(like, l)
+                                if name not in out:
+                                    tsize = [npars_this]
+                                    [tsize.append(d) for d in pred.shape]
+                                    out[name] = np.zeros(tsize)
+                                out[name][count] = pred
+                            
+                        else:
+                            attr = emu_info['likelihood'][like]
+                            pred = getattr(model.likelihood[like], attr)
+                            name = '{}.{}'.format(like, attr)
+                            if name not in out:
+                                tsize = [npars_this]
+                                [tsize.append(d) for d in pred.shape]
+                                out[name] = np.zeros(tsize)
+                            out[name][count] = pred
+                count += 1
+
+
+    for k in out:        
+        with h5py.File('{}.{}'.format(emu_info['output_filename'], rank), 'w') as fp:
             for k in out:
-                tot = out[k]
-                shape = tot.shape
+                shape = out[k].shape
                 fp.create_dataset(k, shape)
-                fp[k][:] = tot
+                fp[k][:] = out[k]            
 
+    comm.Barrier()
+    if rank==0:
+        with h5py.File(emu_info['output_filename'], 'w') as fp:
+            for k in out:
+                for n in range(nproc):
+                    fp['{}_{}'.format(k, n)] = h5py.ExternalLink('{}.{}'.format(emu_info['output_filename'], n), k)
 
 if __name__ == '__main__':
 
@@ -98,11 +135,28 @@ if __name__ == '__main__':
 
     bounds = model.prior.bounds()
     param_names = model.prior.params
-    pars = AnzuParameters(bounds[:, 0], bounds[:, 1])
+
     emu_info = info['emulate']
     nstart = emu_info.pop('nstart', 0)
     nend = emu_info.pop('nend', 100)
+    param_names_fast = emu_info.pop('param_names_fast', None)
+    nfast = emu_info.pop('nfast_per_slow', 1)
 
-    generate_models(pars, param_names, model, emu_info, nstart=nstart, nend=nend)
+    if param_names_fast is not None:
+        param_names = list(set(param_names) - set(param_names_fast))
+        fast_idx = [model.prior.params.index(f) for f in param_names_fast]
+        slow_idx = [model.prior.params.index(f) for f in param_names]
+
+        bounds_fast = bounds[fast_idx]
+        bounds_slow = bounds[slow_idx]
+
+        params = AnzuParameters(bounds_slow[:, 0], bounds_slow[:, 1])
+        params_fast = AnzuParameters(bounds_fast[:, 0], bounds_fast[:, 1])
+    else:
+        params_fast = None
+    
+    generate_models(params, param_names, model, emu_info, nstart=nstart, nend=nend,
+                    params_fast=params_fast, param_names_fast=param_names_fast,
+                    nfast_per_slow=nfast)
 
 
